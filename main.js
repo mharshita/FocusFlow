b86635c648eb084082e42f841390a01ea51c1e3c
Harshita Mangla <manglaharshita@gmail.com>
2023-10-01 18:54:18 +0530

    add input for block website page

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const redirect = document.getElementById("redirect");
 const websiteInput = document.getElementById("websiteInput");
 const websiteInputBtn = document.getElementById("websiteInputBtn");
 const blockedWebsites = document.getElementById("blockedWebsites");
+var blockedWebsitesArray = [];
 
 function navigation() {
   if (
@@ -16,8 +17,39 @@ function navigation() {
   }
 }
 
+function updateBlockedWebsiteUi(blockedWebsitesArray) {
+  blockedWebsites.innerHTML = "";
+
+  for (let i = 0; i < blockedWebsitesArray.length; i++) {
+    var pElement = document.createElement("p");
+    pElement.textContent = blockedWebsitesArray[i];
+    blockedWebsites.appendChild(pElement);
+  }
+}
+
 window.onload = function () {
   navigation();
+
+  //check if the key focus_flow_blocked is present in the chrome storage api or not
+  //if it is not present then initialize the key with an empty array
+  chrome.storage.local.get("focus_flow_blocked", function (result) {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    } else {
+      if ("focus_flow_blocked" in result) {
+        //key is present
+        blockedWebsitesArray = result.focus_flow_blocked;
+        updateBlockedWebsiteUi(blockedWebsitesArray);
+      } else {
+        //key is not present
+        chrome.storage.local.set({ focus_flow_blocked: [] }, function () {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          }
+        });
+      }
+    }
+  });
 };
 
 redirectBtn.addEventListener("click", (e) => {
@@ -31,8 +63,12 @@ blockBtn.addEventListener("click", (e) => {
 });
 
 websiteInputBtn.addEventListener("click", (e) => {
-  //console.log(websiteInput.value);
   //TODO: Do the following:
-  //If website is invalid, show error
-  //If website is valid, add the website in the array that contains blocked website in the storage
+  //If website is invalid show error
+  // If website is valid add the website in the array that contains blocked website in the storage
+  // add the website in the accepted format
+  blockedWebsitesArray.push(websiteInput.value);
+  chrome.storage.local.set({ focus_flow_blocked: blockedWebsitesArray });
+  updateBlockedWebsiteUi(blockedWebsitesArray);
+  websiteInput.value = "";
 });
